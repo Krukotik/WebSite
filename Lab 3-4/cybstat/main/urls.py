24ead7d00c3732d1d5d5c41a66fc3645f0f4d393
Ваше Имя <rostislav1001@gmail.com>
2022-06-22 01:15:15 +0300

    design work

--- Lab 3-4/cybstat/main/urls.py
+++ Lab 3-4/cybstat/main/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('about', About.as_view()),
+    path('reg', Reg.as_view()),
+    path('log', Log.as_view()),
 ]
 
--- Lab 3-4/cybstat/main/views.py
+++ Lab 3-4/cybstat/main/views.py
@@ -7,7 +7,14 @@ class About(TemplateView):
     template_name = 'main/about.html'
 
 
+class Reg(TemplateView):
+    template_name = 'main/reg.html'
+
+
+class Log(TemplateView):
+    template_name = 'main/log.html'
+
+
 def index(request):
     players = CybdbModels.objects.order_by('-id')
     return render(request, 'main/index.html', {'CybdbModels': players})
-
